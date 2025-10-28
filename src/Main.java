e7d3a5648f509e7c72b1dc3a95f3e2e80f6d39af
Jakub969 <jakub.hrubizna@gmail.com>
2025-10-28 19:54:41 +0100

    predcasne ukoncenie delete AVL

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
             testerAVL.fillAndEmptyStructure();
             testerAVL.randomOperationGenerator();
         }*/
-        long seed = 1761287891971L;
+        long seed = 1761677549130L;
         Structure_Tester<GenerateData> testerAVL = new Structure_Tester<>(new AVL<GenerateData>(), seed, 20);
         testerAVL.fillAndEmptyStructure();
         testerAVL.randomOperationGenerator();
